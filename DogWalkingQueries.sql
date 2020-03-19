735ece0daecf4e67a03c69b93d4418d82a2dc32e
aborgra <audrey.borgra@gmail.com>
2020-03-19 14:19:09 -0500

    first commit

---
+++ DogWalkingQueries.sql
@@ -0,0 +1,49 @@
+﻿SELECT o.[Name], n.[Name]
+from [Owner] o
+LEFT JOIN Neighborhood n on o.NeighborhoodId = n.Id;
+
+SELECT o.[Name], n.[Name]
+from [Owner] o
+LEFT JOIN Neighborhood n on o.NeighborhoodId = n.Id
+WHERE o.Id = 4;
+
+SELECT [Name]
+from Walker
+ORDER BY [Name] desc;
+
+SELECT DISTINCT Breed
+from Dog;
+
+SELECT d.[Name] as 'Dog Name', o.[Name] as 'Owner Name', n.[Name] as 'Neighborhood Name'
+from Dog d
+LEFT JOIN [Owner] o on d.OwnerId = o.Id
+LEFT JOIN Neighborhood n on o.NeighborhoodId = n.Id;
+
+SELECT COUNT(OwnerId) as 'Dog Count', o.[Name] 
+from Dog d
+LEFT JOIN [Owner] o on d.OwnerId = o.Id	
+GROUP BY OwnerId, o.[Name];
+
+SELECT COUNT(WalkerId) as 'Walker', w.[Name]
+from Walks wa
+LEFT JOIN Walker w on wa.WalkerId = w.Id
+GROUP BY WalkerId, w.[Name];
+
+SELECT COUNT(NeighborhoodId) as 'Neighborhood Walkers Count', n.[Name] as 'Neighborhood Name'
+from Walker w
+LEFT JOIN Neighborhood n on w.NeighborhoodId = n.Id
+GROUP BY NeighborhoodId, n.[Name];
+
+SELECT d.[Name]
+from Dog d
+LEFT JOIN Walks w on d.Id = w.DogId
+WHERE w.Date > '03/12/2020' and w.Date < '03/20/2020';
+
+SELECT d.[Name]
+from Dog d
+LEFT JOIN Walks w on d.Id = w.DogId
+GROUP BY d.[Name]
+HAVING COUNT(w.Id) = 0;
+
+
+
---
+++ InitializedDatabaseInfo.sql
@@ -0,0 +1,63 @@
+﻿CREATE TABLE Neighborhood (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(55) NOT NULL
+);
+
+CREATE TABLE [Owner] (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(55) NOT NULL,
+    [Address] VARCHAR(55) NOT NULL,
+    NeighborhoodId INTEGER NOT NULL,
+    Phone VARCHAR(55) NOT NULL,
+    CONSTRAINT FK_Owner_Neighborhood FOREIGN KEY(NeighborhoodId) REFERENCES Neighborhood(Id)
+);
+
+CREATE TABLE Walker (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(55) NOT NULL,
+    NeighborhoodId INTEGER NOT NULL,
+    CONSTRAINT FK_Walker_Neighborhood FOREIGN KEY(NeighborhoodId) REFERENCES Neighborhood(Id)
+);
+
+
+CREATE TABLE Dog (
+    Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+    [Name] VARCHAR(55) NOT NULL,
+    OwnerId INTEGER NOT NULL,
+    Breed VARCHAR(55) NOT NULL,
+    Notes VARCHAR(55) NOT NULL,
+    CONSTRAINT FK_Dog_Owner FOREIGN KEY(OwnerId) REFERENCES [Owner](Id)
+);
+
+CREATE TABLE Walks (
+   Id INTEGER NOT NULL PRIMARY KEY IDENTITY,
+   Date dateTime NOT NULL,
+   Duration INTEGER NOT NULL,
+   WalkerId INTEGER NOT NULL,
+   DogId INTEGER NOT NULL,
+   CONSTRAINT FK_Walks_Dog FOREIGN KEY(DogId) REFERENCES Dog(Id),
+   CONSTRAINT FK_Walks_Walker FOREIGN KEY(WalkerId) REFERENCES Walker(Id)
+);
+
+
+INSERT INTO Neighborhood ([Name]) VALUES ('Tulsa');
+INSERT INTO Neighborhood ([Name]) VALUES ('Jenks');
+INSERT INTO Neighborhood ([Name]) VALUES ('Utah');
+
+INSERT INTO Walker([Name], NeighborhoodId) VALUES ('Kevin', 1);
+INSERT INTO Walker([Name], NeighborhoodId) VALUES ('Jansen', 2);
+
+INSERT INTO [Owner] ([Name], [Address], NeighborhoodId, Phone) VALUES ('Mike', '5003 E 99th st', 1,'918-728-1111');
+INSERT INTO [Owner] ([Name], [Address], NeighborhoodId, Phone) VALUES ('Madi', '123123123', 2,'918-728-1111');
+INSERT INTO [Owner] ([Name], [Address], NeighborhoodId, Phone) VALUES ('Heidi', 'Somewhere in utah', 3,'918-728-1111');
+INSERT INTO [Owner] ([Name], [Address], NeighborhoodId, Phone) VALUES ('Garrett', 'Knoxville', 3,'918-728-1111');
+
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES ('Buddy', 1, 'Husky','Very Happy Lazy Dog');
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES ('Luna', 2, 'Husky','Very Happy Hyper Dog');
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES ('Apollo', 2, 'Labradoodle','IDK never met him');
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES ('Hazel', 3, 'American BullDog','Very Happy Lazy Dog');
+INSERT INTO Dog ([Name], OwnerId, Breed, Notes) VALUES ('Lucy', 4, 'Boston Terrier','Very Happy Lazy Dog');
+
+INSERT INTO Walks ([Date], Duration, WalkerId, DogId) VALUES ('01/01/2020', 30, 1,1);
+INSERT INTO Walks ([Date], Duration, WalkerId, DogId) VALUES ('01/02/2020', 40, 1,1);
+
